6416b8056e3747b079cfae11035033e4a10f375f
Vijay choudhary <vijaychoudharyin@gmail.com>
2024-09-19 22:18:39 +0530

    added devise and login

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  def create
+    resource = User.find_by(username: params[:user][:username])
+    if resource && resource.valid_password?(params[:user][:password])
+      sign_in(resource_name, resource)
+      
+    else
+      flash[:alert] = 'Invalid username or password.'
+      render :new
+    end
+  end
+  
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
